24e87aba6c01a55ee5f283728fac4e54b09ba25d
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2017-12-16 23:44:33 -0500

    got the JS file to link correctly

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -1,8 +1,10 @@
+console.log("test to see if this page shows up");
+
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
 
 $.ajax({
   url: queryURL,
   method: 'GET'
-}).done(function(response) {
-  console.log(response);
+}).done(function(data) {
+  console.log("I got data", data);
 });
