7289ef42eb51dff5e8dd6ad88f9dd92e3c472928
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2017-12-17 07:44:10 -0500

    Fav buttons are added

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -5,8 +5,23 @@ var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
 $.ajax({
   url: queryURL,
   method: 'GET'
-}).done(function(data) {
+})
+.done(function(data) {
   console.log("I got data", data);
 });
 
 var topics =["cheerleading", "too cute", "I love Lucy", "Tea cup Pigs"]
+
+function showButtons() {
+
+    for (let index = 0; index < topics.length; index++) {
+        
+        var a = $("<Button>");
+        a.text(topics[index]);
+        $(".favButtons").append(a);
+        
+    }
+}
+
+    showButtons();
+
