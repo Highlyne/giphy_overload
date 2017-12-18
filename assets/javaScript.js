92ae6f83a394518c563ad5d634ed89f84e2c24bb
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2017-12-17 22:58:10 -0500

    more buttons

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -1,26 +1,9 @@
 console.log("test to see if this page shows up");
 
-$(".favButtons").on("click", dumpGiphy);
-
-function dumpGiphy() {
-
-    console.log("Testing buttons");
-//   var giphy = $(this).attr("data-name");
-//   var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
-
-// $.ajax({
-//   url: queryURL,
-//   method: 'GET'
-// })
-// .done(function(data) {
-//   console.log("I got data", data);
-// });
-}
-
 var topics =["cheerleading", "too cute", "I love Lucy", "Tea cup Pigs"]
 
 function showButtons() {
-
+ 
     for (let index = 0; index < topics.length; index++) {
         
         var a = $("<Button>");
@@ -29,8 +12,22 @@ function showButtons() {
         
     }
 }
-
-   
-
     showButtons();
+
+
+// function dumpGiphy() {
+
+//   var favTopics =["cheerleading", "too+cute", "i+love+lucy", "tea+cup+pigs"];
+//   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + favTopics[0] "&api_key=dc6zaTOxFJmzC&limit=10";
+
+//   $.ajax({
+//   url: queryURL,
+//   method: 'GET'
+// })
+// .done(function(repsonse) {
+//   console.log("I got data" + data);
+// });
+// }
+
+
 
